4ef8079eb9b4ba338b1d232443797fd5fce8cb28
Christian Cepeda <christianuz@outlook.com>
2026-01-10 21:14:58 -0500

    Initial Commit

---
+++ ImagenResizer/ImagenResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagenResizerApp.swift
+//  ImagenResizer
+//
+//  Created by Christian Cepeda on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ImagenResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
